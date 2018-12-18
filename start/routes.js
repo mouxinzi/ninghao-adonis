ed07814f57021d63b42dd91be06bd2215824dcc6
t.k <408509669@qq.com>
2018-12-18 10:07:53 +0800

    'req-res'

--- start/routes.js
+++ start/routes.js
@@ -17,3 +17,60 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+
+//Route.get('/posts', ({ request }) => request.get())
+
+//Route.post('/posts',({ request })=>request.post())
+//Route.post('/posts', ({ request }) => request.all())
+//Route.post('/posts', ({ request }) => request.only(['title','content']))
+//Route.post('/posts', ({ request }) => request.expect(['title', 'content']))
+//Route.post('/posts', ({ request }) => request.input('title', 'content'))
+//Route.post('/posts', ({ request }) => request.only(['title','content']))
+//Route.post('/posts', ({ request }) => request.collect(['title', 'content']))
+
+//Route.get('/posts', ({ request }) => request.headers())
+// Route.get('/posts', ({ request }) => request.header('user-agent'))
+// Route.get('/posts', ({ request, response}) => {
+//   response.cookie('theme','dark')
+//   response.clearCookie('theme')
+//   return request.cookie('theme','light')
+// })
+
+// Route.get('/posts',({ response })=>{
+//   // response.send('List of posts.')
+//   return {
+//     title:'List of posts.'
+//   }
+// })
+
+
+// const delay = (data,time)=>{
+//   return new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//       resolve(data)
+//     }, time)
+//   })
+// }
+
+// Route.get('/posts', async ({ response }) => {
+//   const data = await delay(
+//     'List of posts ~',
+//     3000
+//   )
+//   return  data
+// })
+
+Route.get('/list-of-posts',({response})=>{
+  //response.redirect('/posts',true,301)
+  response.route('list-of-posts')
+})
+
+Route.get('/list-of-food-posts', ({ response }) => {
+  //response.redirect('/posts',true,301)
+  response.route('list-of-posts',{category:'food'})
+})
+
+Route.get('/posts/:category?',({ params})=>{
+  return `List of ${params.category || 'default'} posts.`
+})
+.as('list-of-posts')
